348071a023194e7c5580b4ae15c7826859ec8707
apo-ll <marsappollo3@gmail.com>
2024-02-27 19:29:45 +0300

    HELLO

--- app/Details/[media_type]/[id]/page.jsx
+++ app/Details/[media_type]/[id]/page.jsx
@@ -1,11 +1,11 @@
 // app/Details/[...slug]/page.jsx
 
-'use client'
+"use client";
 
-import { useParams } from 'next/navigation'
-import { useQuery } from '@tanstack/react-query';
-import { Oval } from 'react-loader-spinner';
-import Image from 'next/image';
+import { useParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { Oval } from "react-loader-spinner";
+import Image from "next/image";
 
 export default function Details() {
   const { media_type, id } = useParams();
@@ -13,40 +13,50 @@ export default function Details() {
   console.log(media_type, id);
 
   async function fetchDetails(id, media_type) {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/${media_type}/${id}?language=en-US`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
-          },
-        }
-      );
-      return response.json();
+    const response = await fetch(
+      `https://api.themoviedb.org/3/${media_type}/${id}?language=en-US`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
+        },
+      }
+    );
+    return response.json();
   }
 
   const { data, isLoading } = useQuery({
     queryKey: ["details", id, media_type],
     queryFn: async () => await fetchDetails(id, media_type),
-  })
-
+  });
 
   return (
-    <div>
+    <div className="container">
       {isLoading && (
         <div className="flex items-center justify-center h-[600px]">
           <Oval color="#144056" height={100} width={100} />
         </div>
       )}
-      <div key={data} className='relative'>
-      <Image
-        src={`https://image.tmdb.org/t/p/original${data && data.backdrop_path}`}
-        alt={data && data.original_name}
-        width={1920}
-        height={1080}
-        className="object-cover h-full w-full"
-      />
-      <h1 className='absolute text-white text-3xl top-0'>{data && data.name || data && data.title}</h1>
+      <div
+        key={data}
+        className="relative items-center flex text-center justify-cente"
+      >
+        <Image
+          src={`https://image.tmdb.org/t/p/original${
+            data && data.backdrop_path
+          }`}
+          alt={data && data.original_name}
+          width={900}
+          height={700}
+          className="rounded-lg "
+        />
+        <button className="absolute text-white text-3xl  justify-center">
+          watch trailer
+        </button>
+      </div>
+      <div className="flex flex-col gap-3 mb-10  max-w-2xl">
+        <h1 className="text-3xl mt-10">Overview</h1>
+        <p className="text-wrap ">{data && data.overview}</p>
       </div>
     </div>
   );
